0af9542ab0ebbdb88955e7517073c0245cb7b3c1
RazoTRON <vlad05966@gmail.com>
2023-03-18 02:52:56 +0200

    mvp

---
+++ PhotoGallery/app/src/test/java/org/hyperskill/photogallery/Test.kt
@@ -0,0 +1,25 @@
+package org.hyperskill.photogallery
+
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.test.*
+import androidx.compose.ui.test.junit4.createComposeRule
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.shadows.ShadowLog
+
+@RunWith(RobolectricTestRunner::class)
+class PasswordFieldTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Before
+    @Throws(Exception::class)
+    fun setUp() {
+        ShadowLog.stream = System.out // Redirect Logcat to console
+    }
+
+}
